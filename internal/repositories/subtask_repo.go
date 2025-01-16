96b01b2202c7cade5f7a8f0bb94cd49d5aa281f2
mohammad-khalilpour <kian.khalilpour@gmail.com>
2025-01-16 12:28:58 +0330

    added subtask repository

--- internal/repositories/subtask_repo.go
+++ internal/repositories/subtask_repo.go
@@ -1,34 +1,98 @@
 package repositories
 
-import "mizito/pkg/models"
+import (
+	"errors"
+	"gorm.io/gorm"
+	"mizito/internal/database"
+	"mizito/internal/repositories/utils"
+	"mizito/pkg/models"
+)
 
 type SubtaskRepository interface {
-	GetSubtasksByTask(taskID uint) ([]models.Subtask, error)
-	CreateSubtask(subtask *models.Subtask) (uint, error)
-	GetSubtaskByID(subtaskID uint) (*models.Subtask, error)
-	UpdateSubtask(subtask *models.Subtask) (uint, error)
-	DeleteSubtask(subtask *models.Subtask) (uint, error)
+	GetSubtasksByTask(taskID uint, requestUserID uint) ([]models.Subtask, error)
+	CreateSubtask(subtask *models.Subtask, requestUserID uint) (uint, error)
+	GetSubtaskByID(subtaskID uint, requestUserID uint) (*models.Subtask, error)
+	UpdateSubtask(subtask *models.Subtask, requestUserID uint) (uint, error)
+	DeleteSubtask(subtask *models.Subtask, requestUserID uint) (uint, error)
 }
 
 type subtaskRepository struct {
+	permissionRepo utils.PermissionRepository
+	DB             *gorm.DB
 }
 
-func NewSubtaskRepository() SubtaskRepository {
-	return &subtaskRepository{}
+func NewSubtaskRepository(postgreSql *database.DatabaseHandler) SubtaskRepository {
+	permissionRepo := utils.NewPermissionRepository(postgreSql)
+	return &subtaskRepository{DB: postgreSql.DB, permissionRepo: permissionRepo}
 }
 
-func (sr *subtaskRepository) GetSubtasksByTask(taskID uint) ([]models.Subtask, error) {
-	return nil, nil
+// GetSubtasksByTask fetches all subtasks for a given task if the user is an admin of the task.
+func (sr *subtaskRepository) GetSubtasksByTask(taskID uint, requestUserID uint) ([]models.Subtask, error) {
+	// Check if the user has admin permission for the task
+	if !sr.permissionRepo.CheckUserHasAccessToTask(taskID, requestUserID) {
+		return nil, errors.New("you don't have access to the project")
+	}
+
+	var subtasks []models.Subtask
+	if err := sr.DB.Where("task_id = ?", taskID).Find(&subtasks).Error; err != nil {
+		return nil, err
+	}
+	return subtasks, nil
 }
-func (sr *subtaskRepository) CreateSubtask(subtask *models.Subtask) (uint, error) {
-	return 0, nil
+
+// CreateSubtask creates a new subtask under a task if the user is an admin of the task.
+func (sr *subtaskRepository) CreateSubtask(subtask *models.Subtask, requestUserID uint) (uint, error) {
+	// Check if the user has admin permission for the task
+	if !sr.permissionRepo.CheckUserIsAdminOfTask(subtask.TaskID, requestUserID) {
+		return 0, errors.New("you don't have access to the project")
+	}
+
+	if err := sr.DB.Create(subtask).Error; err != nil {
+		return 0, err
+	}
+	return subtask.ID, nil
 }
-func (sr *subtaskRepository) GetSubtaskByID(subtaskID uint) (*models.Subtask, error) {
-	return nil, nil
+
+// GetSubtaskByID fetches a subtask by its ID if the user is an admin of the associated task.
+func (sr *subtaskRepository) GetSubtaskByID(subtaskID uint, requestUserID uint) (*models.Subtask, error) {
+
+	var subtask models.Subtask
+	if err := sr.DB.First(&subtask, subtaskID).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, gorm.ErrRecordNotFound
+		}
+		return nil, err
+	}
+
+	if !sr.permissionRepo.CheckUserHasAccessToTask(subtask.TaskID, requestUserID) {
+		return nil, errors.New("you don't have access to the project")
+	}
+
+	return &subtask, nil
 }
-func (sr *subtaskRepository) UpdateSubtask(subtask *models.Subtask) (uint, error) {
-	return 0, nil
+
+// UpdateSubtask updates an existing subtask if the user is an admin of the associated task.
+func (sr *subtaskRepository) UpdateSubtask(subtask *models.Subtask, requestUserID uint) (uint, error) {
+	// Check if the user has admin permission for the task
+	if !sr.permissionRepo.CheckUserHasAccessToTask(subtask.TaskID, requestUserID) {
+		return 0, errors.New("you don't have access to the project")
+	}
+
+	if err := sr.DB.Save(subtask).Error; err != nil {
+		return 0, err
+	}
+	return subtask.ID, nil
 }
-func (sr *subtaskRepository) DeleteSubtask(subtask *models.Subtask) (uint, error) {
-	return 0, nil
+
+// DeleteSubtask deletes a subtask if the user is an admin of the associated task.
+func (sr *subtaskRepository) DeleteSubtask(subtask *models.Subtask, requestUserID uint) (uint, error) {
+	// Check if the user has admin permission for the task
+	if !sr.permissionRepo.CheckUserIsAdminOfProject(subtask.TaskID, requestUserID) {
+		return 0, errors.New("you don't have access to the project")
+	}
+
+	if err := sr.DB.Delete(subtask).Error; err != nil {
+		return 0, err
+	}
+	return subtask.ID, nil
 }
